90762403736ca07abfc205e188a3a3bae3efe115
Matt Lee <eelwehttam@gmail.com>
2024-07-29 12:37:40 -0500

    cleaning up files

--- runjava.sh
+++ runjava.sh
@@ -3,11 +3,11 @@
 javac -sourcepath src/java -d build/java src/java/CalculateAverage.java src/java/CalculateAverageTest.java
 
 # time java -cp build/java CalculateAverage ./data/weather_stations.csv
-# time java -Xmx6g -cp build/java CalculateAverage /Users/mlee/Downloads/data/measurements.txt
+time java -Xmx6g -cp build/java CalculateAverage /Users/mlee/Downloads/data/measurements.txt
 # time java -cp build/java CalculateAverage /Users/mlee/Downloads/data/measurements_a.txt
 # time java -cp build/java CalculateAverage /Users/mlee/Downloads/data/measurements_sm.txt
 
 # java -cp build/java CalculateAverageTest
 
 
-jbang --javaagent=ap-loader@jvm-profiling-tools/ap-loader=start,event=cpu,file=profile.html src/java/CalculateAverage.java /Users/mlee/Downloads/data/measurements.txt
+# jbang --javaagent=ap-loader@jvm-profiling-tools/ap-loader=start,event=cpu,file=profile.html src/java/CalculateAverage.java /Users/mlee/Downloads/data/measurements.txt
